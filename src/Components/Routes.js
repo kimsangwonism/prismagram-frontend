9459b18ca647fa086e4ad5d0f5a74102819d8d0e
kim sang won <ksw_mac@kims-MacBook.local>
2019-06-11 09:21:19 +0900

    9.2 logout complete

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,24 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Auth from "../Routes/Auth";
 import Feed from "../Routes/Feed";
 import Explore from "../Routes/Explore";
 import SearchContainer from "../Routes/Search/index";
-import Profile from "../Routes/Profile";
+import ProfileContainer from "../Routes/Profile/index";
 
 const LoggedInRoutes = () => (
     <Switch>
         <Route exact path="/" component={Feed} />
         <Route path="/explore" component={Explore} />
         <Route path="/search" component={SearchContainer} />
-        <Route path="/:username" component={Profile} />
+        <Route path="/:username" component={ProfileContainer} />
+        <Redirect from="*" to="/" />
     </Switch>    
 );
 
 const LoggedOutRoutes = () => (
     <Switch>
         <Route exact path="/" component={Auth} />
+        <Redirect from="*" to="/" />
     </Switch>    
 );
 
